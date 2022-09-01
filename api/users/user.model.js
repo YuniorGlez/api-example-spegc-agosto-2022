b3b1e0196669cdf722cfd9a3f4e2f9bb692b0a76
YuniorGlez <yunior@squaads.com>
2022-09-01 13:56:48 +0100

    Añadimos un mejor Schema de mongoose y añadimos sobretodo validaciones en el User

--- api/users/user.model.js
+++ api/users/user.model.js
@@ -1,10 +1,34 @@
 var mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-    username: String,
-    name: String,
-    email: String,
-    tweetsIDs: [String]
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required : [true, 'El username no puede estar en blanco'],
+        minLength : [4 , 'El username tiene que tener mínimo 4 caracteres' ],
+        maxLength : [120 , 'El username tiene que tener máximo 120 caracteres' ],
+        unique: [true, 'El username {VALUE} está ya repetido']
+    },
+    name: {
+        type: String,
+        minLength : 4,
+        maxLength : 200
+    },
+    email: {
+        type: String,
+        required : true,
+        minLength : 8,
+        maxLength : 120,
+        validate: {
+            validator: function(loQueLLega) {
+              return loQueLLega.includes('@') && loQueLLega.endsWith('.com');
+            },
+            message: 'el email {VALUE} no está bien formateado'
+          },
+    },
+    tweetsIDs: [ {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'tweet' 
+    } ]
 });
 
 const UserModel = mongoose.model('user', UserSchema);
